37c0aea5bb0f6de16a4e75183f5695e96f6e76fc
juliettecezzar <jc@juliettecezzar.com>
2019-04-19 00:31:16 -0400

    changes to pull down data

--- API-test/myscript.js
+++ API-test/myscript.js
@@ -9,15 +9,18 @@ var humidity; //Humidity, %
 var weathertext; //Group of weather parameters (Rain, Snow, Extreme etc.)
 var weatherdescription; //Weather condition within the group
 var cloudiness; //Cloudiness, %
-
+var istanbul_id = 745042;
+var roswell_id = 5488441;
 
 $(document).ready(function(){
 
 
     $.ajax({
-        url: 'http://api.openweathermap.org/data/2.5/weather?id=745042&units=metric&APPID=be611e3081d65abcf9f2dead869ee338', //CHANGE THE URL TO YOUR API QUERY
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=' + istanbul_id + '&units=metric&APPID=be611e3081d65abcf9f2dead869ee338', //CHANGE THE URL TO YOUR API QUERY
         dataType: 'jsonp',
         success: function(results){
+          console.log("results");
+          console.log(results);
             weathertext = results.weather[0].main;
             weatherdescription = results.weather[0].description;
             windspeed = results.wind.speed;
@@ -29,27 +32,50 @@ $(document).ready(function(){
             humidity = results.main.humidity;
             cloudiness = results.clouds.all;
 
-            myWeatherInterpretation();
+            myWeatherInterpretation('istanbul');
+
+        }
+    });
+
+    $.ajax({
+        url: 'http://api.openweathermap.org/data/2.5/weather?id=' + roswell_id + '&units=metric&APPID=be611e3081d65abcf9f2dead869ee338', //CHANGE THE URL TO YOUR API QUERY
+        dataType: 'jsonp',
+        success: function(results){
+          console.log("results");
+          console.log(results);
+            weathertext = results.weather[0].main;
+            weatherdescription = results.weather[0].description;
+            windspeed = results.wind.speed;
+            winddeg = results.wind.deg;
+            tempmin = results.main.temp_min;
+            tempmax = results.main.temp_max;
+            temp = results.main.temp;
+            pressure = results.main.pressure;
+            humidity = results.main.humidity;
+            cloudiness = results.clouds.all;
+
+            myWeatherInterpretation('roswell');
 
         }
     });
 
 
-    function myWeatherInterpretation(){
+    function myWeatherInterpretation(city){
         //THIS IS WHERE YOU CAN CUSTOMIZE YOUR PAGE'S FUNCTIONS
-
+        console.log(city);
 
         //JUST SEEING IF THE VALUES COME THROUGH
-        $('p').append('windspeed: ' + windspeed);
-        $('p').append('winddeg: ' + winddeg);
-        $('p').append('temp: ' + temp);
-        $('p').append('tempmin: ' + tempmin);
-        $('p').append('tempmax: ' + tempmax);
-        $('p').append('pressure: ' + pressure);
-        $('p').append('humidity: ' + humidity);
-        $('p').append('weathertext: ' + weathertext);
-        $('p').append('weatherdescription: ' + weatherdescription);
-        $('p').append('cloudiness: ' + cloudiness);
+        $('.'+city).append(city + ': ');
+        $('.'+city).append('windspeed: ' + windspeed + ' | ');
+        $('.'+city).append('winddeg: ' + winddeg + ' | ');
+        $('.'+city).append('temp: ' + temp + ' degrees celsius ');
+        // $('p').append('tempmin: ' + tempmin);
+        // $('p').append('tempmax: ' + tempmax);
+        // $('p').append('pressure: ' + pressure);
+        // $('p').append('humidity: ' + humidity);
+        // $('p').append('weathertext: ' + weathertext);
+        // $('p').append('weatherdescription: ' + weatherdescription);
+        // $('p').append('cloudiness: ' + cloudiness);
 
 
 
